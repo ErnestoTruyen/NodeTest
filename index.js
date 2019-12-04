06d354f6c7b1285e2673a78e3c1a29fe4cd5c624
Ernesto Truyen <ernestoseisa@outlook.com>
2019-12-04 00:45:29 -0300

    prueba

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader('Content-type','text/plain');
-    res.end('Hello world\n');
+    res.end('Hello world Ernesto\n');
 });
 
 server.listen(PORT,()=>{
